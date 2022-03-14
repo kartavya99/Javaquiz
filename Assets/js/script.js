55eeb1d8a629b2225372d53124dcb3ecfc44b812
kartavya <kartavyas@gmail.com>
2022-03-14 14:55:48 +1100

    structured Assets folder

---
+++ Assets/js/script.js
@@ -0,0 +1,37 @@
+var question = [{
+question: "Inside which HTML element do we put the JavaScript?",
+options: ["<script>" , "<js>" , "<javascript>" , "<scripting>"],
+correctAnswer: "<script>",
+},
+
+{
+question : "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+correctAnswer : "if(!=5)",
+options:["if(i<>5)" , "if(!=5)>" , "if i<>5" , "if i=!5 then"],
+correctAnswer : "if(!=5)",
+},
+
+{
+question : "Which operator is used to assign a value to a variable?",
+options:[ "-", "x" , "=" , "*"], 
+correctAnswer : "=",
+},
+
+{
+question : "Is javaScript case-sensitive?",
+options:["No" , "Yes" , "Not applicable" , "Both A and B"],
+correctAnswer : "Yes",
+},
+
+{
+question : "How do you declare a JavaScript variable?",
+correctAnswer : "var carName;",
+options:[ "v carName;" , "variable carName;" , "var carName;" , "var = carName;"]
+}
+];
+
+
+
+
+
+
