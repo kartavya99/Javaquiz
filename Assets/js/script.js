2d538c54a99c2cdfa4c5d5bb28822622a115368e
kartavya <kartavyas@gmail.com>
2022-03-15 17:42:25 +1100

    updated JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,7 +10,6 @@ correctAnswer: "<script>",
 
 {
 question : "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-correctAnswer : "if(!=5)",
 options:["if(i<>5)" , "if(!=5)>" , "if i<>5" , "if i=!5 then"],
 correctAnswer : "if(!=5)",
 },
@@ -35,20 +34,23 @@ options:[ "v carName;" , "variable carName;" , "var carName;" , "var = carName;"
 ];
 
 // timer variable
-var secondsLeft = 100;
+
 // variable to store date from question array 
 var shuffledQuestion = question;
 var randomQuestion = ""
-
+var quizTracker = 0;
+var secondsLeft = 101;
+var userAnswer = 0;
 
 //below are the variable to initiate the quiz
 var startButton = document.getElementById("start-btn");
 var questionContainerElement = document.getElementById("question-container");
 var heading = document.getElementById("heading");
 var rulesBox = document.getElementById("rules-box");
-var quizTimer = document.getElementById("quiz-timer");
+var timerCount = document.getElementById("timer-count");
 var question = document.getElementById("question");
 var answerButton = document.getElementById("answer-buttons");
+var quizTimer = document.getElementById("quiz-timer");
 
 
 // added click event listener to start button
@@ -57,6 +59,7 @@ startButton.addEventListener("click", startGame);
 
 
 function startGame() {
+    secondsLeft = 101;
     console.log("Started");
     startButton.classList.add("hide"); // after clicking the start button - hide it to start the quiz
     heading.classList.add("hide"); // hide the header
@@ -64,48 +67,54 @@ function startGame() {
     questionContainerElement.classList.remove("hide"); // question will pop up
     quizTimer.classList.remove("hide");
     setNextQuestion();
-
+    startTimer();
+// trigeer stat timer function
     
 }
 
 //function to set the first question 
 function setNextQuestion() {
-    var showQuestion = [(shuffledQuestion)];
+    var showQuestion = shuffledQuestion[quizTracker].question;
     console.log(showQuestion);
+    question.textContent = showQuestion; 
+    answerButton.innerHTML = "";
+    for (i=0; i < shuffledQuestion[quizTracker].options.length; i++){
+        var backhandButton = document.createElement("button");
+        backhandButton.textContent = shuffledQuestion[quizTracker].options[i];
+        backhandButton.onclick = checkAnswer;
+        answerButton.append(backhandButton);
+        
+    }
 }
 
+function checkAnswer() {
+    console.log("checkingAnswer");
+    console.log(this.textContent);
+    // conditions to check correct answer with if 
+//    var userAns = 
+  
+
+//    if(userAns === correctAns){
+//        return true;
+//    } else {
+//        return false;
+//    }
+    
 
 
+    //quiz tracker goes up one by +
+    // excute setNextQuestion 
+}
 
-// var randomQuestion = ""
-//     for (var i = 0; i < shuffledQuestion; i ++) {
-//         var randomQuestion = shuffledQuestion[Math.floor(Math.random()*shuffledQuestion.length)]
-//         randomQuestion += randomQuestion
-//     }
-
-
-
-// //function will start timer for the quiz
-// function startTimer() {
-//     secondsLeft = secondsLeft -1;
-//     if()
-
-//         if(secondsLeft = 0){
-//             clearInterval(timerInterval);
-//             sendMessage() = " thank you for taking this quiz!!!";
-//             startGame();
-//         }
-
-//     }, 1000);
-// }
-
-// function startTimer(){
-//     secondsLeft = secondsLeft - 1;
-//     if (secondsLeft < 100 ){
-//         timerCount.innerHTML = secondsLeft;
-//     }
-//     if (secondsLeft < 1) {
-//         window.clearInterval(update);
-//     }
-// }
-// update = setInterval("startTimer()", 1000)
+//function will start timer for the quiz
+function startTimer() {
+    secondsLeft = secondsLeft - 1;
+    if(secondsLeft < 101) {
+        timerCount.innerHTML = secondsLeft;
+    }
+    if (secondsLeft < 1 ) {
+        window.clearInterval(update);
+    }
+}
+    update = setInterval("startTimer()", 1000);
+    
